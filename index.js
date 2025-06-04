451ad5d995d73093fd6294ad2284063980759067
ssbombaguy <giorgimaisuradze2008@gmail.com>
2025-06-04 18:58:55 +0400

    login register logic

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
+import dotenv from "dotenv";
+dotenv.config();
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
+import { connectToDatabase } from "./services/database.js";
+import authRoutes from "./routes/auth.routes.js";
+
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json());
+connectToDatabase()
+    .then(() => console.log(" Connected to MongoDB"))
+    .catch((err) => console.error(" MongoDB connection failed:", err));
+
+
 app.use(cors());
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(express.static('public'))
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static("public"));
+
+
+app.use("/api/auth", authRoutes);
+
+
+app.get("/", (req, res) => {
+    res.json({ message: "API is running " });
+});
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
